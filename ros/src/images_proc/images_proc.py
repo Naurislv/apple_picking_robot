d486b335b257e4bb5051fb5490c24da021745111
agrist <agris.traskovs@gmail.com>
2017-11-01 09:39:35 -0700

    single tab spacing

--- ros/src/images_proc/images_proc.py
+++ ros/src/images_proc/images_proc.py
@@ -3,18 +3,18 @@ import time
 import rospy
 
 class ImagesProc(object):
-	def __init__(self):
-		rospy.init_node('image_proc')
-		rospy.loginfo('starting image stream')
-		imageStream = ' testing'
+    def __init__(self):
+        rospy.init_node('image_proc')
+        rospy.loginfo('starting image stream')
+        imageStream = ' testing'
 
-	def loop(self):
-	       	rate = rospy.Rate(20) 
-        	i = 0
-        	while not rospy.is_shutdown():
-            		i += 1
-            		rospy.loginfo('image iter: %d', i)
-            		rate.sleep()
+    def loop(self):
+        rate = rospy.Rate(20) 
+        i = 0
+        while not rospy.is_shutdown():
+            i += 1
+            rospy.loginfo('image iter: %d', i)
+ 	    rate.sleep()
 
 if __name__ == '__main__':
     try:
