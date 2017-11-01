a149ad48233d8a66b9173d0c0e655d604f5b925b
agrist <agris.traskovs@gmail.com>
2017-11-01 09:23:15 -0700

    updated files permissions and tabbing in script

--- ros/src/images_proc/images_proc.py
+++ ros/src/images_proc/images_proc.py
@@ -1,24 +1,20 @@
+#!/usr/bin/env python
 import time
 import rospy
 
-def class ImagesProc(object):
-	imageStream = 'placeholder'
+class ImagesProc(object):
 	def __init__(self):
 		rospy.init_node('image_proc')
 		rospy.loginfo('starting image stream')
-		imageStream = initImagestream()
+		imageStream = ' testing'
 
-	def initImageStream(self):
-		return 'image stream init'
-
-
-def loop(self):
-        rate = rospy.Rate(20) 
-        i = 0
-        while not rospy.is_shutdown():
-            i += 1
-            rospy.loginfo('image iter: %d', i)
-            rate.sleep()
+	def loop(self):
+	       	rate = rospy.Rate(20) 
+        	i = 0
+        	while not rospy.is_shutdown():
+            		i += 1
+            		rospy.loginfo('image iter: %d', i)
+            		rate.sleep()
 
 if __name__ == '__main__':
     try:
